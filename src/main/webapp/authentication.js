542ba564112712b86c38dc0d50c74af513fcccf7
Viktor Klijn <viktor.klijn@student.hu.nl>
2023-06-15 16:18:54 +0200

    Uncommented authentication.js because of fixes made to the verifyToken function in AuthService.js

--- src/main/webapp/authentication.js
+++ src/main/webapp/authentication.js
@@ -1,39 +1,39 @@
-// import {verifyToken} from "./service/AuthService.js";
-//
-// const token = sessionStorage.getItem("WMSToken");
-//
-// let username;
-// if (token) {
-//     verifyToken(token)
-//         .then((usernm) => {
-//             username = usernm;
-//         })
-//         .catch((error) => {
-//             console.log(error.toString());
-//         })
-// }
-//
-//
-// switch (location.pathname) {
-//     case "/overview.html":
-//         if (token === null) {
-//             location.assign("/index.html")
-//         }
-//         else {
-//             if (!username) {
-//                 window.sessionStorage.clear();
-//                 location.assign("/index.html");
-//             }
-//         }
-//         break;
-//     case "":
-//     case "/":
-//     case "/index.html":
-//         if (token !== null) {
-//             if (username) {
-//                 location.assign("/overview.html")
-//                 window.sessionStorage.setItem("username", username);
-//             }
-//         }
-//         break;
-// }
+import {verifyToken} from "./service/AuthService.js";
+
+const token = sessionStorage.getItem("WMSToken");
+
+let username;
+if (token) {
+    verifyToken(token)
+        .then((usernm) => {
+            username = usernm;
+        })
+        .catch((error) => {
+            console.log(error.toString());
+        })
+}
+
+
+switch (location.pathname) {
+    case "/overview.html":
+        if (token === null) {
+            location.assign("/index.html")
+        }
+        else {
+            if (!username) {
+                window.sessionStorage.clear();
+                location.assign("/index.html");
+            }
+        }
+        break;
+    case "":
+    case "/":
+    case "/index.html":
+        if (token !== null) {
+            if (username) {
+                location.assign("/overview.html")
+                window.sessionStorage.setItem("username", username);
+            }
+        }
+        break;
+}
