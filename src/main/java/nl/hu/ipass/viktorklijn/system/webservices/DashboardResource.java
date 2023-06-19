f3e8d5781777163a8ce16f06d8dab091280ffcc7
Viktor Klijn <viktor.klijn@student.hu.nl>
2023-06-19 13:06:28 +0200

    Function name changed, now a POST function that checks who the user is since the articles are now loaded from a specific store that the user is connected to.

--- src/main/java/nl/hu/ipass/viktorklijn/system/webservices/DashboardResource.java
+++ src/main/java/nl/hu/ipass/viktorklijn/system/webservices/DashboardResource.java
@@ -1,27 +1,39 @@
 package nl.hu.ipass.viktorklijn.system.webservices;
 
+import io.jsonwebtoken.JwtException;
+import nl.hu.ipass.viktorklijn.system.auth.LoginRequest;
+import nl.hu.ipass.viktorklijn.system.auth.VerificationToken;
 import nl.hu.ipass.viktorklijn.system.models.Article;
 import nl.hu.ipass.viktorklijn.system.models.Shop;
+import nl.hu.ipass.viktorklijn.system.models.User;
 
 import javax.annotation.security.RolesAllowed;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.*;
 
+import static nl.hu.ipass.viktorklijn.utils.JwtUtil.verifyToken;
+
 @Path("/dashboard")
 public class DashboardResource {
 
-    @GET
+    @POST
     @Path("/articles")
     @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
 //    @RolesAllowed("admin")
-    public Response loadDashboard() {
-        List<Article> articles = Shop.getArticles();
-        return Response.ok(articles).build();
+    public Response getArticles(VerificationToken verificationToken) {
+        try {
+            Map<String, String> verificationResponse = verifyToken(verificationToken);
+
+            List<Article> articles = User.getUser(verificationResponse.get("username")).getShop().getArticles();
+            return Response.ok(articles).build();
+        } catch (JwtException e) {
+            return Response.status(Response.Status.UNAUTHORIZED).build();
+        }
     }
+
 }
 
 
