7e55b07023d6269a357ac4197e4cccdc1f976396
Viktor Klijn <viktor.klijn@student.hu.nl>
2023-06-21 14:48:20 +0200

    New class added for stock change requests.

---
+++ src/main/java/nl/hu/ipass/viktorklijn/system/models/ProductUpdate.java
@@ -0,0 +1,9 @@
+package nl.hu.ipass.viktorklijn.system.models;
+
+public class ProductUpdate {
+    public int stock;
+    public int articleNumber;
+    public String token;
+
+    public ProductUpdate(){};
+}
