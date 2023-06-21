75cc2d7daf12b53f25e831b0357eb540bb4c45d2
Viktor Klijn <viktor.klijn@student.hu.nl>
2023-06-21 14:47:39 +0200

    UpdateArticle service added that communicates with backend to change the stock of an article

--- src/main/webapp/service/ArticleService.js
+++ src/main/webapp/service/ArticleService.js
@@ -6,9 +6,21 @@ export default class articleService {
      * @returns {Promise<array<Article>>}
      */
     static getArticles() {
-        const url = `${baseURL}/dashboard/articles/`;
-        console.log(JSON.stringify({"token": sessionStorage.getItem("WMSToken")}))
-        return fetch(url, {method: "POST", body: JSON.stringify({"token": sessionStorage.getItem("WMSToken")}), headers: {'Content-Type': 'application/json' }})
+        const URL = `${baseURL}/dashboard/articles/`;
+        return fetch(URL, {method: "POST", body: JSON.stringify({"token": sessionStorage.getItem("WMSToken")}), headers: {'Content-Type': 'application/json' }})
+            .then((response) => response.json());
+    }
+
+    static updateArticle() {
+        const URL = `${baseURL}/dashboard/update`;
+        const FORM = document.querySelector("#dialog-form");
+        const FORMDATA = new FormData(FORM);
+        let jsonData = {};
+
+        FORMDATA.forEach((value, key) => jsonData[key] = value);
+        jsonData["token"] = sessionStorage.getItem("WMSToken");
+
+        return fetch(URL, {method: "POST", body: JSON.stringify(jsonData), headers: {'Content-Type': 'application/json' }})
             .then((response) => response.json());
     }
 }
