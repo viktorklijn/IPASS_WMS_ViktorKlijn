82aeaf38d5f7a1c2704de288f7689cb85f0fac5b
Viktor Klijn <viktor.klijn@student.hu.nl>
2023-06-21 14:49:50 +0200

    Added new queryselectors.

--- src/main/webapp/view/TableResource.js
+++ src/main/webapp/view/TableResource.js
@@ -1,4 +1,5 @@
 import articleService from "../service/ArticleService.js";
+import openDialog from "./DialogResource.js";
 
 /**
  *
@@ -10,21 +11,21 @@ function renderArticle(article) {
     const CLONE = TEMPLATE.content.cloneNode(true);
     const ELEMENT = CLONE.querySelector("#table-template tr#table-row");
 
-    // ELEMENT.id = article.name;
     CLONE.querySelector(".product-number").textContent = `${article.articleNumber}`;
     CLONE.querySelector(".product-name").textContent = article.name;
     CLONE.querySelector('.product-manufacturer').textContent = article.manufacturer.name;
     CLONE.querySelector(".product-category").textContent = article.category.name;
     CLONE.querySelector(".product-price").textContent = article.price;
     CLONE.querySelector(".product-barcode").innerHTML = `<img src="https://barcodeapi.org/api/auto/${article.barcode}" alt="Barcode: ${article.barcode}" height="100vh">`;
-
+    CLONE.querySelector(".product-stock").innerHTML = article.stock;
+    CLONE.querySelector(".edit-stock button").addEventListener("click", (event) => openDialog(article.articleNumber, article.stock));
 
     return CLONE;
 }
 
 function render() {
-    const articleTable = document.getElementById("overview-table");
-    articleTable.innerHTML = "";
+    const articleTable = document.getElementById("table-body");
+    // articleTable.innerHTML = "";
     articleService.getArticles()
         .then((articles) => {
             articles.forEach((article) => {
