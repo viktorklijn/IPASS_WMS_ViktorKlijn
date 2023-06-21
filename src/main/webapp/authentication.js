0ffa81da1d86cdc918a5acc1bb589e97a0bd96b4
Viktor Klijn <viktor.klijn@student.hu.nl>
2023-06-21 14:47:06 +0200

    Made script await tokenverification to get rid of errors.

--- src/main/webapp/authentication.js
+++ src/main/webapp/authentication.js
@@ -2,9 +2,9 @@ import {verifyToken} from "./service/AuthService.js";
 
 const token = sessionStorage.getItem("WMSToken");
 
-let username;
-if (token) {
-    verifyToken(token)
+let username = null;
+if (token !== null) {
+    await verifyToken(token)
         .then((usernm) => {
             username = usernm;
         })
@@ -19,18 +19,18 @@ switch (location.pathname) {
         if (token === null) {
             location.assign("/index.html")
         }
-        // else {
-        //     if (!username) {
-        //         window.sessionStorage.clear();
-        //         location.assign("/index.html");
-        //     }
-        // }
+        else {
+            if (username === null) {
+                window.sessionStorage.clear();
+                location.assign("/index.html");
+            }
+        }
         break;
     case "":
     case "/":
     case "/index.html":
         if (token !== null) {
-            if (username) {
+            if (username !== null) {
                 location.assign("/overview.html")
                 window.sessionStorage.setItem("username", username);
             }
