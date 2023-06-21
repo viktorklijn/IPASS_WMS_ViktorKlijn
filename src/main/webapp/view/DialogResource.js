2aac49c9b9e566d736f024850ea1c1f687a1d7f7
Viktor Klijn <viktor.klijn@student.hu.nl>
2023-06-21 14:49:08 +0200

    New resource that fills dialog with correct info.

---
+++ src/main/webapp/view/DialogResource.js
@@ -0,0 +1,16 @@
+import ArticleService from "../service/ArticleService.js";
+
+/**
+ *
+ * @param {Number}articleNumber
+ * @param {Number}articleStock
+ */
+export default function openDialog(articleNumber, articleStock) {
+    const DIALOG = document.querySelector("dialog#edit-stock-dialog");
+
+    DIALOG.querySelector("#stock-input").value = articleStock;
+    DIALOG.querySelector("#article-number-input").value = articleNumber;
+    DIALOG.querySelector(".change-button").addEventListener("click", ArticleService.updateArticle)
+
+    DIALOG.showModal();
+}
