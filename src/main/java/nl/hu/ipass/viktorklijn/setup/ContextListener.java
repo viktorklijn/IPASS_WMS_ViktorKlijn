10cb595adf4d8742f069bebe0580ee0a5d10e7b9
Viktor Klijn <viktor.klijn@student.hu.nl>
2023-06-22 22:45:40 +0200

    Updated save and load data functions.

--- src/main/java/nl/hu/ipass/viktorklijn/setup/ContextListener.java
+++ src/main/java/nl/hu/ipass/viktorklijn/setup/ContextListener.java
@@ -7,14 +7,18 @@ import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 import java.io.*;
 import java.util.ArrayList;
-import java.util.List;
 
 @WebListener
 public class ContextListener implements ServletContextListener {
-    private static final String DATA_FILE = "data.txt";
+    private static final String DATA_FILE = "C:\\Users\\vikto\\Desktop\\Projects\\IPASS-magazijn\\src\\main\\resources\\data.obj";
 
-        @Override
+    @Override
     public void contextInitialized(ServletContextEvent sce) {
+        loadData();
+//        initializeData();
+    }
+
+    private void initializeData() {
         Shop s1 = new Shop(1, "ByJensen");
         Manufacturer m1 = new Manufacturer("humdakin", "Denmark", "23975954");
         Category c1 = new Category("Meubels");
@@ -29,41 +33,43 @@ public class ContextListener implements ServletContextListener {
 
         Role r1 = new Role("admin", 1);
         User u1 = new User("Viktor", "test@test.com", "pass", 12, r1, s1);
-
-        saveData();
-
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
         saveData();
-        System.out.println("saved");
     }
 
     private void saveData() {
-        try (FileOutputStream fos = new FileOutputStream(DATA_FILE);
-             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
-            oos.writeObject(Shop.getShops());
-            System.out.println("saved");
+        try {
+            FileOutputStream fos = new FileOutputStream(DATA_FILE);
+             ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(new SaveModel(Shop.getShops(), User.getUsers()));
+            oos.close();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private List<Shop> loadData() {
+    private void loadData() {
         ArrayList<Shop> shops = new ArrayList<>();
+        ArrayList<User> users = new ArrayList<>();
+        SaveModel data;
 
         File file = new File(DATA_FILE);
         if (file.exists()) {
             try (FileInputStream fis = new FileInputStream(DATA_FILE);
                  ObjectInputStream ois = new ObjectInputStream(fis)) {
-                shops = (ArrayList<Shop>) ois.readObject();
+                data = (SaveModel) ois.readObject();
+                System.out.println(data.getShops());
+                Shop.setShops(data.getShops());
+                User.setUsers(data.getUsers());
+                System.out.println("loaded");
             } catch (IOException | ClassNotFoundException e) {
                 e.printStackTrace();
             }
         }
 
-        return shops;
     }
 }
